0e0e9c4a91644a00a0cf946fe0d7931328918323
Tom Mitchell <mitcht032@gmail.com>
2020-11-04 15:09:42 -0800

    added server file

---
+++ server.js
@@ -0,0 +1,31 @@
+// Node Dependencies
+var express = require('express');
+//var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+
+//Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+var router = require('./controllers/burgers_controllers.js');
+app.use('/', router);
+
+// Open Server
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+ 
