e44db0eb04d6126e60aa2ccea9c2822cef4c6ce9
Tom Mitchell <mitcht032@gmail.com>
2020-11-04 15:29:16 -0800

    added database

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// Node Dependency
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    port     :  3306,
+    user     : 'root',
+    password : 'Brontoe13', 
+    database : 'burgers_db' 
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+
+// Export the Connection
+module.exports = connection;
---
+++ db/seedb.sql
@@ -0,0 +1,9 @@
+-- Seed Burgers into Database 
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Big Mac", false, "2020-11-04 04:20:00"),
+  ("Whooper", false, "2020-11-04 04:20:01"),
+  ("Cheezburger", false, "2020-11-04 04:20:02"),
+  ("Baconator", false, "2020-11-04 16:20:00"),
+  ("Quarter Pounder", false, "2020-11-04 16:20:01"),
+  ("McRoyal", false, "2020-11-04 04:20:20"),
+  ("Double Double", true, "2020-11-04 16:20:16");
