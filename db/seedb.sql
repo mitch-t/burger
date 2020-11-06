90e4001c1761315a551e6f1b3eb52487e6e88fee
Tom Mitchell <mitcht032@gmail.com>
2020-11-06 14:09:53 -0800

    Delete seedb.sql

--- db/seedb.sql
+++
@@ -1,9 +0,0 @@
--- Seed Burgers into Database 
-INSERT INTO burgers(burger_name, devoured)
-VALUES ("Big Mac", false),
-  ("Whooper", false),
-  ("Cheezburger", false),
-  ("Baconator", false),
-  ("Quarter Pounder", false),
-  ("McRib", false)
-  ("Double Double", true);
