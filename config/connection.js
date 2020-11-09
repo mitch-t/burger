1b6704cd6e213d2964da9c8366a48bb0940d777e
Tom Mitchell <mitcht032@gmail.com>
2020-11-09 13:30:46 -0800

    still trying to connect to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,19 @@
-// Set up MySQL connection.
-const mysql = require("mysql");
+var mysql = require("mysql");
+var connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Brontoe13",
-  database: "burgers_db"
-});
+// For Heroku Deployment vs. Local MySQL Database
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : "localhost",
+    user     : "root",
+    password : "Brontoe13", 
+    database : "burgers_db" 
+  });
+}
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
+// Export the Connection
 module.exports = connection;
--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var app = express();
 //const path = require("path");
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
+app.use(express.static('views/images'));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('views/images'));
+
 //var methodOverride = require('method-override')
 // Set Handlebars.
 const exphbs = require("express-handlebars");
@@ -19,6 +19,7 @@ app.set("view engine", "handlebars");
 //app.get("/", function(req, res) {
  //res.json(path.join(__dirname, "public/index.html"));
 //});
+
 // Import routes and give the server access to them.
 const routes = require("./controllers/burgers_controller");
 
