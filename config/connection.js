5a9030d3d9e08626a95434c7fc7c69667c3fd77b
Tom Mitchell <mitcht032@gmail.com>
2020-11-12 09:37:52 -0800

    still trying heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 var mysql = require("mysql");
 var connection;
-JAWSDB_URL='mysql://vp2681doft4pi9b5:arz16nedukrzqnii@nwhazdrp7hdpd4a4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ydyz362rfmlkfice'
+//JAWSDB_URL='mysql://vp2681doft4pi9b5:arz16nedukrzqnii@nwhazdrp7hdpd4a4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ydyz362rfmlkfice'
 // For Heroku Deployment vs. Local MySQL Database
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else{
+//if(process.env.JAWSDB_URL){
+ // connection = mysql.createConnection(process.env.JAWSDB_URL);
+//}
+//else{
   connection = mysql.createConnection({
-    host     : "localhost",
-    user     : "root",
-    password : "Brontoe13", 
-    database : "burgers_db" 
+    host     : "nwhazdrp7hdpd4a4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user     : "vp2681doft4pi9b5",
+    password : "arz16nedukrzqnii", 
+    database : "ydyz362rfmlkfice" 
   });
-}
+
 
 
 // Export the Connection
