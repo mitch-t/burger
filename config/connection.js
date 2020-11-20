d7905142bc7ac3c41ed8c1d2faa6dea6623fd166
Tom Mitchell <mitcht032@gmail.com>
2020-11-19 16:10:58 -0800

    connecting to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,28 @@
 //require("dotenv").config();
 var mysql = require("mysql");
-var connection;
+let connection;
 
-// For Heroku Deployment vs. Local MySQL Database
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else{
   connection = mysql.createConnection({
     host     : "localhost",
+    port     : 3306,
     user     : "root",
     password : "Brontoe13", 
     database : "burgers_db" 
-  });
-}
+  })
+
+};
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 module.exports = connection;
-
 
--- server.js
+++ server.js
@@ -1,31 +1,48 @@
-const express = require("express");
+var express = require("express");
 //require("dotenv").config();
 var PORT = process.env.PORT || 3000;
 var app = express();
-var bodyParser = require("body-parser")
 
 
-//const path = require("path");
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-app.use(express.static('views/images'));
+
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(bodyParser.json());
+
 
 // Set Handlebars.
-const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require("./controllers/burgers_controller");
+var routes = require("./controllers/burgers_controller");
 
 app.use(routes);
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
